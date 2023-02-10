efeda8aed494be39f48b31f1eee733b05ab566e2
esweeney13 <122942750+esweeney13@users.noreply.github.com>
2023-02-10 18:02:01 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+ Author: Erik Sweeney
+ Date: 2/10/23
+
+ 
+*/
+
+using System;
+
+namespace Arrays
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int[] fibSeq = {0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368};
+
+            for (int i = 0; i < 25; i++)
+            {
+                Console.WriteLine("Fibonacci Number (" + i + ") = " + fibSeq[i]);
+            }
+        }
+    }
+}
+
+
